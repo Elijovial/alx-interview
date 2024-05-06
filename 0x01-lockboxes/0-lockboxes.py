9d067e4c4e48c21aa0dd24f80d8f83c82127cbc7
Elijovial <omeehelijah345@gmail.com>
2024-05-05 19:41:02 -0700

    Task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,18 +1,30 @@
 #!/usr/bin/python3
-"""
-Define lockboxes
+"""A python module that determines if all boxes can be opened
+   from a list of lists
 """
 
 
 def canUnlockAll(boxes):
     """
-    determines if all the boxes can be opened
+    Determines if all the boxes can be opened.
     """
-    keys = [0]
-    for key in keys:
-        for val in boxes[key]:
-            if val not in keys:
-                keys.append(val)
-    if len(keys) == len(boxes):
-        return True
-    return False
+    unlocked = [False] * len(boxes)  # Track unlocked status for each box
+    unlocked[0] = True  # The first box is unlocked
+    keys = set(boxes[0])  # Start with keys from the first box
+
+    # Keep track of boxes we have checked to avoid infinite loops
+    checked_boxes = set()
+
+    while keys:
+        new_keys = set()
+        for key in keys:
+            if key < len(boxes) and not unlocked[key]:
+                # Unlock the box and mark it as checked
+                unlocked[key] = True
+                checked_boxes.add(key)
+                # Add new keys from the unlocked box
+                new_keys.update(boxes[key])
+        # Update keys with the new keys found
+        keys = new_keys.difference(checked_boxes)
+
+    return all(unlocked)
