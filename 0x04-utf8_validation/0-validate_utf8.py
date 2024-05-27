037b810394092363c06f80dde8b37d92fcd9129e
Elijovial <omeehelijah345@gmail.com>
2024-05-26 20:03:10 -0700

    Task 0

---
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This module contains a method to validate UTF-8 encoding.
+"""
+
+
+def validUTF8(data):
+    """
+    Validates whether the given data set represents a valid UTF-8 encoding.
+"""
+    num_bytes = 0
+    mask1 = 1 << 7
+    mask2 = 1 << 6
+
+    for num in data:
+        mask = 1 << 7
+        if num_bytes == 0:
+            while mask & num:
+                num_bytes += 1
+                mask = mask >> 1
+
+            if num_bytes == 0:
+                continue
+
+            if num_bytes == 1 or num_bytes > 4:
+                return False
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+
+        num_bytes -= 1
+
+    return num_bytes == 0
